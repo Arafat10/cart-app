e09a7f3ff16ce0b006956f1779cd0c25552909a9
Arafat10 <arafat.nabil.10@gmail.com>
2024-01-09 22:11:16 +0600

    products showing in cart

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import { PRODUCTS } from '../../products'
+import { Product } from '../shop/product'
+import { ShopContext } from '../../context/shop-context'
 
 export const Cart = () => {
+  const {cartItems} = useContext(ShopContext);
   return (
-    <div>C</div>
+    <div className='cart'>
+      <div>
+        <h1>Your Cart Items</h1>
+        <div className='cartItems'>
+          {" "}
+          {PRODUCTS.map((product)=> {
+            if (cartItems[product.id] !==0){
+              return <CartItem data ={product} />;
+            }
+          }
+          )
+          }
+        </div>
+      </div>
+    </div>
   )
 }
